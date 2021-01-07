0896daf90f14ef6427be8041d219b973e38f5c27
Jyoti Prajapati <52569075+jyotiprajapati98@users.noreply.github.com>
2021-01-07 06:16:03 +0530

    Half Pyramid Print by user input

---
+++ HalfPyramid.java
@@ -0,0 +1,25 @@
+/*
+		*
+		**
+		***
+		****
+		****
+*/
+import java.util.Scanner;
+class HalfPyramid{
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Rows and Column for Half Pyramid , row nad column should equal");
+		int rows = scan.nextInt();
+		int column = scan.nextInt();
+		
+		//Logic for pattern
+		for(int i=0;i<rows;i++){
+			for(int j=0;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println("\n");
+		}
+		
+	}
+}
